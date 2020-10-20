a87d7833a61e7a728da4b95d7705db39f8617fcc
xuyuejun <lwxuyuejun@163.com>
2020-10-20 21:33:40 +0900

    inti code

---
+++ common.js
@@ -0,0 +1,11 @@
+/** 
+ * 初始化整数数组
+ * @param length 数组长度
+ */
+function initIntegerArray(length) {
+    var integerArray = [];
+    for (let i = 1; i <= length; i++) {
+        integerArray.push(i);
+    }
+    return integerArray;
+}
