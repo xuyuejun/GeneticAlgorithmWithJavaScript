8c6752ec5a43eb7d8bff2f4f678c685ba3e32012
xuyuejun <lwxuyuejun@163.com>
2020-10-20 22:18:36 +0900

    add random function

--- GeneticAlgorithm.js
+++ GeneticAlgorithm.js
@@ -1,10 +1,16 @@
 /** 整数数组长度 */
 var integerArrayLength = 10;
+
 /** 任务集合 */
 var tasks = [];
 
+/** 染色体数量 */
+var chromosomeNum = 10;
+
+
 (function initGA(_integerArrayLength) {
     /** 初始化任务集合 */
-    tasks = initIntegerArray(integerArrayLength)
+    tasks = initIntegerArray(integerArrayLength);
+    
 
 })(integerArrayLength)
--- common.js
+++ common.js
@@ -9,3 +9,42 @@ function initIntegerArray(length) {
     }
     return integerArray;
 }
+
+/**
+ * 获取指定范围内的随机数
+ * @param min 最小值
+ * @param max 最大值
+ */
+function getRandom(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+/**
+ * 随机生成不重复数组
+ * @param len 数组长度
+ * @param start 起始位置
+ * @param end 结束为止
+ */
+function getRandomArr(len, start, end) {
+    let arr = [];
+    while (arr.length < len) {
+        let num = getRandom(start, end);
+        if (arr.indexOf(num) == -1) {
+            arr.push(num);
+        }
+    }
+    return arr;
+}
+
+/**
+ * 转换为二进制字符串并补0
+ * @param length 长度
+ * @param num 传入数字
+ */
+function convertToBinary(length, num) {
+    var str = num.toString(2);
+    while (str.length < length) {
+        str = "0" + str;
+    }
+    return str;
+}
